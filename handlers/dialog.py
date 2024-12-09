b85d24cbc76ca05b88e33cea83f6d725dee0c1f4
aruuuuuuke <askatovaaruuke657@gmail.com>
2024-12-09 18:36:03 +0600

    delete

--- handlers/dialog.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, types, F
-from aiogram.filters import Command
-
-opros_router = Router()
-
-@opros_router.message(Command("opros"))
-async def start_opros(message: types.Message):
-    await message.answer("Как вас зовут?")
